f0c752144a8ff13d1e781a4d3bf8103f5cef0f3c
Drake-Z <drake.z.merlin@gmail.com>
2017-11-16 17:16:25 +0800

    修改 .travis.yml

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Drake-Z
 # @Date:   2017-11-15 21:00:45
-# @Last Modified time: 2017-11-16 15:47:34
+# @Last Modified time: 2017-11-16 16:50:57
 
 import os
 import sys
