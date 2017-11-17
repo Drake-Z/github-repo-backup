cd6f2cd2769f5c29119324b610fae7f55dc6f854
Drake-Z <drake.z.merlin@gmail.com>
2017-11-17 19:52:50 +0800

    修改 manage.py 为 git bundle 导出 repo .travis.yml 增加清空 repos-backup

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Drake-Z
 # @Date:   2017-11-15 21:00:45
-# @Last Modified time: 2017-11-17 16:58:17
+# @Last Modified time: 2017-11-17 19:50:34
 
 import os
 import yaml
@@ -80,58 +80,76 @@ def clone_repo(repo_list):
         excute(cmd=cmd)
 
         logger.debug("clone {dir_name} other branch".format(dir_name=dir_name))
-        branchs, sha1 = match_branch(dir_path=dir_path)
         try:
-            os.chdir(dir_path)
-        except FileNotFoundError:
-            logger.info(("{dir_path} 文件夹不存在！！请确认 {dir_name} 项目没有删除。"
-                         "克隆 {dir_path} 失败\n\n"
-                         ).format(dir_path=dir_path, dir_name=dir_name))
+            sha1 = pull_other_branch(dir_name=dir_name, dir_path=dir_path)
+        except Exception as e:
+            logger.info(repr(e))
             continue
-        logger.debug("cd 到 repo 文件夹: " + os.getcwd())
-        for remote_branch in branchs:
-            cmd = ("git branch --track {local_branch} {remote_branch}"
-                   ).format(local_branch=remote_branch.split("/")[1], remote_branch=remote_branch)
-            excute(cmd=cmd)
-        excute(cmd="git fetch --all")
-        excute(cmd="git pull --all")
-        os.chdir("../..")
-        logger.debug("已 cd 到主文件夹: " + os.getcwd())
 
-        excute(cmd="rm -rf {dir_path}*zip".format(dir_path=dir_path))
-        logger.debug("{dir_name} 删除之前的压缩包".format(dir_name=dir_name))
-        filepath = zip_repo(dir_path=dir_path, sha1=sha1) + ".zip"
-
-        logger.debug("压缩包大小: {size}".format(size=int(os.path.getsize(filepath) / 1024 / 1024)))
-        logger.debug("是否分割: {bool}".format(bool=int(os.path.getsize(filepath) / 1024 / 1024) > 95))
-        if int(os.path.getsize(filepath) / 1024 / 1024) > 95:
-            zip_dir = filepath[:-4] + " zip"
-            os.makedirs(zip_dir)
-            cmd = ("zipsplit -n 99614720  -b {zip_dir} {filepath}"
-                   ).format(filepath=filepath.replace(" ", "\ "), zip_dir=zip_dir.replace(" ", "\ "))
-            excute(cmd=cmd)
-            logger.debug("压缩文件夹下文件:\n" + str(os.listdir(zip_dir)))
-            assert bool(os.listdir(zip_dir)) is True, "压缩文件夹下没有文件！"
-            excute(cmd="rm -rf {filepath}".format(filepath=filepath.replace(" ", "\ ")))
-            assert os.path.exists(filepath) is False, filepath + " 没有被删除"
-
-        logger.debug("压缩 {dir_name} 完毕".format(dir_name=dir_name))
+        filename = export_repo(dir_name=dir_name, dir_path=dir_path, sha1=sha1)
+        filepath = "repos-backup/{filename}".format(filename=filename)
+        logger.debug("导出 {dir_name} 完毕".format(dir_name=dir_name))
         excute(cmd="rm -rf {dir_path}".format(dir_path=dir_path))
+        split_file(dir_name=dir_name, filepath=filepath)
+
         assert os.path.exists(dir_path) is False, dir_path + " 没有被删除"
 
         logger.info("repo {dir_name} clone 完毕\n\n".format(dir_name=dir_name))
     return None
 
 
-def zip_repo(dir_path, sha1):
-    logger.debug("开始压缩 {dir_path}".format(dir_path=dir_path))
-    name = ("{dir_path} @ {sha1} {date}"
-            ).format(dir_path=dir_path, sha1=sha1, date=str(datetime.now())[:-10].replace(":", "."))
+def pull_other_branch(dir_name, dir_path):
+    branchs, sha1 = match_branch(dir_path=dir_path)
+    try:
+        os.chdir(dir_path)
+    except FileNotFoundError:
+        raise Exception(("{dir_path} 文件夹不存在！！请确认 {dir_name} 项目没有删除。"
+                         "克隆 {dir_path} 失败\n\n"
+                         ).format(dir_path=dir_path, dir_name=dir_name))
+    logger.debug("cd 到 repo 文件夹: " + os.getcwd())
+    for remote_branch in branchs:
+        cmd = ("git branch --track {local_branch} {remote_branch}"
+               ).format(local_branch=remote_branch.split("/")[1], remote_branch=remote_branch)
+        excute(cmd=cmd)
+    excute(cmd="git fetch --all")
+    excute(cmd="git pull --all")
+    os.chdir("../..")
+    logger.debug("已 cd 到主文件夹: " + os.getcwd())
+    return sha1
+
+
+def export_repo(dir_name, dir_path, sha1):
+    excute(cmd="rm -rf {dir_path}*bundle".format(dir_path=dir_path))
+    logger.debug("{dir_name} 删除之前的导出包".format(dir_name=dir_name))
+    logger.debug("开始导出 {dir_path}".format(dir_path=dir_path))
+    filename = ("{dir_name}_@{sha1}_{date}.bundle"
+                ).format(dir_name=dir_name,
+                         sha1=sha1,
+                         date=str(datetime.now())[:-10].replace(":", "."))
     # py_zip_file.main(zip_path=[dir_path], zip_name=name)
-    cmd = "zip -r9 {name}.zip {dir_path}/*".format(name=name.replace(" ", "\ "), dir_path=dir_path)
+    os.chdir(dir_path)
+    cmd = "git bundle create ../{filename} --all".format(filename=filename)
     excute(cmd=cmd)
-    logger.debug("{dir_path} 压缩完毕".format(dir_path=dir_path))
-    return name
+    os.chdir("../..")
+    logger.debug("{dir_path} 导出完毕".format(dir_path=dir_path))
+    return filename
+
+
+def split_file(dir_name, filepath):
+    logger.debug("导出包大小: {size}".format(size=int(os.path.getsize(filepath) / 1024 / 1024)))
+    logger.debug("是否分割: {bool}".format(bool=int(os.path.getsize(filepath) / 1024 / 1024) > 95))
+    if int(os.path.getsize(filepath) / 1024 / 1024) > 95:
+        zip_dir = filepath + "_bundle"
+        os.makedirs(zip_dir)
+        cmd = ("split -b 95M -d -a 3 {filepath} {zip_dir}/{dir_name}"
+               ).format(filepath=filepath,
+                        zip_dir=zip_dir,
+                        dir_name=dir_name)
+        excute(cmd=cmd)
+        logger.debug("导出文件夹下文件:\n" + str(os.listdir(zip_dir)))
+        assert bool(os.listdir(zip_dir)) is True, "导出文件夹下没有文件！"
+        excute(cmd="rm -rf {filepath}".format(filepath=filepath))
+        assert os.path.exists(filepath) is False, filepath + " 没有被删除"
 
 
 def get_repo_list():
